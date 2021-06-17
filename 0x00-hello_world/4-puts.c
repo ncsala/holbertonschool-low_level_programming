1b924b049b7b9c2878f26e3ccd93d3882e0ef955
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-17 12:06:44 -0700

    successfully

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 
-/*
- *main - Prints exactly \"Programming is like building a multilingual puzzlee
+/*main - Prints exactly \"Programming is like building a multilingual puzzlee
  */
 
 int main(void)
