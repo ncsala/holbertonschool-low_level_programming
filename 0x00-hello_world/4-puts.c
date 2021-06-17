6684a0cb5e3d6d669453158e5593fb1f09565b26
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-17 12:00:50 -0700

    successfully

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /*
- *Write a C program that prints exactly \"Programming is like building a multilingual puzzle, followed by a new line
+ *Write a C program that prints exactly \"Programming is like building a multilingual puzzlee
  */
 
 int main(void)
