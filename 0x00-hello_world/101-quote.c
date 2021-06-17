7f624894fb08a4c648a228f69d87fdc8d363ab1f
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-17 16:47:02 -0700

    successfully

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-	char txt []  = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char txt[]  = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, txt, strlen(txt));
 	return (1);
 }
