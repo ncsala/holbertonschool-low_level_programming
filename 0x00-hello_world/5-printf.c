386ed1d7241e2d41e2ee764e8b92defaca27fc8d
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-17 12:42:30 -0700

    successfully

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include<stdio.h>}
+#include<stdio.h>
 
 /**
  * main - print a string
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,"0)
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return(0);
 }
