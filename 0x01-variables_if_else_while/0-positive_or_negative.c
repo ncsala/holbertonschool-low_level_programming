daeb6b8106da396005049c8b125aa7180b5a5a4f
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-18 07:41:47 -0700

    successfully

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include<time.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ * main - p
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if(n>0)
+		printf("%d is positive\n", n);
+	else if(n<0)
+		printf("%d is negative\n", n);
+	else if(n==0)
+		printf("%d is zero\n", n);
+	return (0);
+}
