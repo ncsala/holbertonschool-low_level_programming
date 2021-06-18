67a8b9d4518cf9d9f8540899ecc78f97a99d9660
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-18 12:02:09 -0700

    successfully

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - prints digits from 0 to 9
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	while (n = 0; n < 10; n++) 
+	{
+		printf("%d", n);
+	}
+	return(0);
+}
+
