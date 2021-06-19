bc0cf04ef1683114e8cd7f784ee65b9ada5d81da
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-18 17:35:45 -0700

    successfully

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - prints 
+ * Return: 0
+ */
+
+int main(void)
+{
+	char hexa = 48;
+
+	do {
+		putchar(hexa);
+		hexa++;
+	} while (hexa < 58);	
+	hexa = 97;
+	do {
+		putchar(hexa);
+		hexa++;
+	} while (hexa < 103);
+	putchar('\n');
+	return (0);
+}
+
+
