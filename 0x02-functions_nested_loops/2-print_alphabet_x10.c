f45e748b6836191a7275d3aa7689353683ded8ea
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-22 11:52:39 -0700

    successfully

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/*
+ * print_alphabet_x10 - prins alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int alltheletters = 97;
+	int contador = 1;
+
+	for (; contador < 10; contador++)
+	{
+		while (alltheletters <= 122)
+		{
+			_putchar(alltheletters);
+			alltheletters += 1;
+		}
+		alltheletters = 97;
+		
+		_putchar('\n');
+	}
+}
