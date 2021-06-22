93539c0026ae8046785f6ad0147809024d44d5cb
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-22 13:11:29 -0700

    successfully

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+include "holberton.h"
+
+/**
+ * int _isalpha - checks for alphabetic character.
+ *
+ * Return: Returns 1 if c is a letter, lowercase or uppercase
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && <= 122)
+		return (1);
+	else
+		return (0);
+
+	_putchar('\n');
+}
