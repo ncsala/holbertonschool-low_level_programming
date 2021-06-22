153a6cb142e9f7e75817a52f597a17c531426318
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-22 10:59:41 -0700

    successfully

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -10,10 +10,10 @@ int main(void)
 	char happyholbies[] = "Holberton";
 	int r;
 
-	for (r=0 ; happyholbies[r] != '\0'; r++)	
+	for (r=0 ; happyholbies[r] != '\0'; r++)
 	{
 		_putchar(happyholbies[r]);
-	}	
+	}
 	_putchar('\n');
 
 	return (0);
