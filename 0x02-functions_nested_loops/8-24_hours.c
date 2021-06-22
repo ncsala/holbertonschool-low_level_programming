4fa9409db49fad75ab6afef8a5597b2ce77baf8f
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-22 16:53:02 -0700

    successfully

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -20,12 +20,12 @@ void jack_bauer(void)
 				{	
 					if (a >= 50 && b >= 52)
 					break;
-					putchar(a);
-					putchar(b);
-					putchar(58);
-					putchar(c);
-					putchar(d);
-					putchar('\n');
+					_putchar(a);
+					_putchar(b);
+					_putchar(58);
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
 				}
 			}
 		}
