150629867792e6249696a0faae537cffa4de84db
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-22 09:44:45 -0700

    successfully

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,38 +7,32 @@
 
 int main(void)
 {
-	int tens, ones, tens2 = '0', ones2 = '0';
+	int tens = '0', ones = '1', tens2 = '0', ones2 = '0';
 		
-	while (tens2 <= '9')
-	{
-		while (ones2 <= '9')
-		{	
-			tens = tens2;	
-			while (tens <= '9')
-			{	
-				ones = ones2 + 1;
-				while (ones <= '9')
+	for (; tens2 <=  '9'; tens2++)
+	{	
+		for (; ones2 <= '9'; ones2++)
+		{
+			for (; tens <= '9'; tens++)
+			{
+				for (ones = tens + 1; ones <= '9'; ones++)
 				{
 					putchar(tens2);
 					putchar(ones2);
-					putchar(' ');
 					putchar(tens);
 					putchar(ones);
-			
-					ones += 1;
-					if (tens < '9')
-					{
-						putchar(',');
-						putchar(' ');
-					}
-
+					putchar(',');
+					putchar(' ');
+					
 				}
-				ones = '0';
-				tens += 1;
+//				ones = '0'; //se tiene q resetear pq sino illega a nueve y se acaban todos los for
 			}
-			ones2 += 1;
+			tens = '0';
 		}
-		tens2 += 1;
+		ones2 = '0';
 	}
+
+
+	
 	putchar('\n');
 }
---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include<holberton.h>
+
+/**
+ * main - prints "Holberton"
+ * Return: 0
+ */
+
+int main(void)
+{
+	char happyholbies[] = "Holberton";
+	int r;
+
+	for (r=0 ; happyholbies[r] != '0'; r++)	
+	{
+		_putchar(happyholbies[r]);
+	}	
+	_putchar('\n);
+
+	return (0);
+}
