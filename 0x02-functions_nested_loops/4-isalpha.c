a25fef0cdff2b47aeee9b543a3e21d2af1c85345
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-22 13:23:16 -0700

    successfully

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,9 +2,12 @@
 
 /**
  * int _isalpha - Write a function that checks for alphabetic character
- *
  * @c: ASCII from a to z and A to Z
  *
+ * Description: Write a function that checks for alphabetic character.
+ * Prototype: int _isalpha(int c);
+ * Returns 1 if c is a letter, lowercase or uppercase
+ * Returns 0 otherwise
  * Return: Returns 1 if c is a letter, lowercase or uppercase
  */
 
