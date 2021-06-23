8195e26161de83a94af19388d5d79a31ec14ada8
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-22 18:31:18 -0700

    successfully

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * add - Write a function that adds two integers and returns the result.
+ *
+ * Return: returns r+s
+ */
+
+int add(int r, int s)
+{
+	return (r + s);
+}
