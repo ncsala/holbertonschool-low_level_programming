7e02d56cf93bad4c02efbd9d9fe2c5ce24bdc321
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-22 17:47:24 -0700

    successfully

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,8 @@
 #include "holberton.h"
 
 /**
- * jack_bauer - Write a function that prints every minute of the day of Jack Bauer, starting 
- * from 00:00 to 23:59.
- * 
+ * jack_bauer - prints every minute of the day, starting from 00:00 to 23:59.
+ *
  * Return: void return
  */
 
