91b5fdad41ecdba4644e71cb288a4457b9e1fb43
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-22 17:33:02 -0700

    successfully

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,23 +9,23 @@
 
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int hour2, hour, min2, min;
 
-	for (a = 48; a <= 50; a++)
+	for (hour2 = 48; hour2 <= 50; a++)
 	{
-		for (b = 48; b <= 57; b++)
+		for (hour = 48; hour <= 57; hour++)
 		{
-			for (c = 48; c <= 53; c++)
+			for (min2 = 48; min2 <= 53; min2++)
 			{
-				for (d = 48; d <= 57; d++)
+				for (min = 48; min <= 57; min++)
 				{
 					if (a >= 50 && b >= 52)
 					break;
-					_putchar(a);
-					_putchar(b);
+					_putchar(hour2);
+					_putchar(hour1);
 					_putchar(58);
-					_putchar(c);
-					_putchar(d);
+					_putchar(min2);
+					_putchar(min);
 					_putchar('\n');
 				}
 			}
