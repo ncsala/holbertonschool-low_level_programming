05f944764c66e61140972173a071b4d91aba5053
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-24 08:36:52 -0700

    successfully

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,10 @@
 
 /**
  * _isupper - Write a function that checks for uppercase character.
- *
+ * @r: save the retunr value
+ * @c: input characte
+ * @c: input character
+ * 
  * Return: 1 if c is uppercase 0 otherwise
  */
 
