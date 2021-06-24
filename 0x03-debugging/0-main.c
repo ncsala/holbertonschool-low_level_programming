8bd18037c19bd452b38897d5a0de51745ab5b532
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-23 20:05:43 -0700

    successfully

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,16 @@
 #include "holberton.h"
 
 /**
- * main - this main file is a test for a postitive_or_negative() function
- *
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
-	int i;
+        int i;
 
-	i = 0;
+        i = 0;
+        positive_or_negative(i);
 
-	positive_or_negative(i);
-
-	return (0);
+        return (0);
 }
