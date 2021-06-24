7303e7cfdaddefa7ba79b9edd081775637af0dd6
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-24 08:47:52 -0700

    successfully

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * int _isdigit - Write a function that checks for a digit (0 through 9)
+ * @c: receive any character
+ * Return: Returns 1 if c is a digit and 0 otherwise
+ */
+
+int __isdigit(int c)
+{
+	return (c > 47 && c < 58);
+}	
