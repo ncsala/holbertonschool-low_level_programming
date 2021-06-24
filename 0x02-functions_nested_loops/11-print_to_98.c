1cc92aa98f5a7c9e3159cff0520be748f90c7162
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-23 19:29:14 -0700

    successfully

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,13 +12,13 @@ void print_to_98(int n)
 	while (n < 98)
 	{
 		printf("%d, ", n);
-		n++;
+		n = n + 1;
 	}
         
 	while (n > 98)
 	{
 		printf("%d, ", n);
-		n--;
+		n = n - 1;
 	}
 
 	if (n == 98)
