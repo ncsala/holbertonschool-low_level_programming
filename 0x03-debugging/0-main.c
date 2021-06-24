6b46840a1104a8806d79961575a9b92c63f9a7ba
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-23 19:52:19 -0700

    successfully

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * main - this main file is a test for a postitive_or_negative() function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	positive_or_negative(0);
+	
+	return (0);
+}
