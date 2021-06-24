f2c8e751aa612ab04237d35008ef783992c24161
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-23 18:51:31 -0700

    successfully

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,7 +17,7 @@ void times_table(void)
 		for (column = 1; column <= 9; column++)
 		{
 			outcome = row * column;
-			
+
 			_putchar(44);
 			_putchar(32);
 
@@ -34,7 +34,7 @@ void times_table(void)
 
 
 		}
-	
+
 	_putchar('\n');
 	}
 }
