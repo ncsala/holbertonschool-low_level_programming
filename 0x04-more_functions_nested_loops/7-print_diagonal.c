1a2ce6693a9c9f9d4931c946168202c20d3fc95f
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-25 19:50:33 -0700

    successfully

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,10 +14,12 @@ void print_diagonal(int n)
 	if (n > 0)
 	{
 		for (aux = 1; aux <= n; aux++)
+		{
 			for (aux2 = 1; aux2 < aux; aux2++)
 				_putchar(' ');
 			_putchar('-');
 			_putchar('\\');
+		}
 
 	}
 	else
