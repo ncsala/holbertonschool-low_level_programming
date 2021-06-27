85e36bf5b3725b9739e9af183abd6290a6e93c3f
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-26 19:12:26 -0700

    successfully

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * main -
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int num1 = 1, num2 = 2, fn = 0, pairfn = 0;
+
+	for (; fn <= 4000000;)
+	{
+		if ((num1 % 2) == 0)
+		{
+			pairfn = pairfn + num1;
+		}
+		fn = num1 + num2;
+		num1 = num2;
+		num2 = fn;
+	}
+
+	printf("%li", pairfn);
+	printf("\n");
+
+	return (0);
+}
