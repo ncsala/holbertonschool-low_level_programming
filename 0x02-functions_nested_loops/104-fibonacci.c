40696ab6fc4adcdd69bbb3366fc7b2ccf2a62d74
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-06-26 19:49:48 -0700

    successfully

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ * main - Write a program that finds and prints the first 98 Fibonacci numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int counter = 1;
+	unsigned int num1 = 1, num2 = 2, fn;
+
+	printf("%u, %u", num1, num2);
+
+	do {
+		fn = num1 + num2;
+		printf(", %#x", fn);
+		num1 = num2;
+		num2 = fn;
+		counter += 1;
+	} while (counter <= 96);
+
+	printf("\n");
+
+	return (0);
+}
